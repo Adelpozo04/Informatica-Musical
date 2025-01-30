019e731390b103d8d9e08b1f69cd6a3edc41c510
(i)nés <ineprimo@ucm.es>
2025-01-30 15:51:18 +0100

    FELIC CUMPLEAÑOS

--- Practica 1/practica1.py
+++ Practica 1/practica1.py
@@ -1,11 +1,15 @@
 # Adrian del Pozo Hernandez
 # Ines Primo Lopez
 
-# % %
+#%%
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
+import kbhit
 import time # para medir tiempos de ejecución
+
+
 # gráficos en el notebook
-%matplotlib inline
+#%matplotlib inline
 SRATE = 44100 # Sample rate, para todo el programa
+# %%
